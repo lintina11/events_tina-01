379540e23ed115e7787db3cb434d48101290abd7
christina <christina@tradeserv.com>
2023-07-28 17:42:09 +0800

    頁面樣式補充

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { ref } from 'vue';
-import { NRow, NCol, NSpace, NButton, NGrid,NGi } from 'naive-ui'
+import { NRow, NCol } from 'naive-ui'
 import topbar from '../components/topbar.vue'
+import bTitle from '../components/b-title.vue'
 import mainIntro from '../components/mainIntro.vue'
 import bMatching from '../components/b-matching.vue'
 import bOdometer from '../components/b-odometer.vue'
+import bSchedule from '../components/b-schedule.vue'
+import bSignup from '../components/b-signup.vue'
 // import test from '../components/test.vue'
 
 
@@ -13,45 +16,32 @@ import bOdometer from '../components/b-odometer.vue'
 <template>
     <topbar />
     <mainIntro/>
+
     <bMatching />
+    <!-- 人次 -->
     <n-row gutter="0">
-        <n-col :span="24">
+        <n-col :span="18" :offset="3">
+            <bTitle msg="我們的夥伴"/>
             <bOdometer />
         </n-col>
     </n-row>
-
-    <!-- <n-space>
-        <n-button>Default</n-button>
-        <n-button type="tertiary">
-            Tertiary
-        </n-button>
-        <n-button type="primary">
-            Primary
-        </n-button>
-        <n-button type="info">
-            Info
-        </n-button>
-        <n-button type="success">
-            Success
-        </n-button>
-        <n-button type="warning">
-            Warning
-        </n-button>
-        <n-button type="error">
-            Error
-        </n-button>
-    </n-space> -->
+    <!-- 行事曆 -->
+    <n-row gutter="0">
+        <n-col :span="18" :offset="3">
+            <bSchedule/>
+        </n-col>
+    </n-row>
+    <!-- 報名 -->
+    <n-row class="b-signup">
+        <n-col :span="10" :offset="7">
+            <bSignup/>
+        </n-col>
+    </n-row>
 </template>
 
 
 <style scoped>
-.light-green {
-    height: 108px;
-    background-color: rgba(0, 128, 0, 0.12);
-}
-
-.green {
-    height: 108px;
-    background-color: rgba(0, 128, 0, 0.24);
+.b-signup{
+    background-color: #f7f7f7;
 }
 </style>
