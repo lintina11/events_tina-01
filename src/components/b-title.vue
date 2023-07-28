6068c7a50530271bacdf628841b720b7bcc2c3f4
christina <christina@tradeserv.com>
2023-07-28 17:39:26 +0800

    通用title

---
+++ src/components/b-title.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <h2>{{ msg }}</h2>
+</template>
+
+<style lang="scss" scoped>
+h2{
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 700;
+    margin: 20px 0;
+}
+</style>
