a3679e5715010d963838146ec2afe74e47c93bc3
christina <christina@tradeserv.com>
2023-08-18 16:41:44 +0800

    test區

---
+++ src/components/test/emit.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps(
+    { count: "count" }
+)
+
+
+</script> 
+
+<template>
+    <div>
+        <!-- 子組件的內容 -->
+        <h2>
+            {{ count }}
+            <button @click="$emit('increaseBy')">點我+1</button>
+            
+        </h2>
+        <!-- v-once -->
+        <span v-once> 原本是 {{count}} </span>
+    </div>
+</template>
+
+
+<style>
+h2,p,span {
+    width: 100%;
+    display: inline-block;
+    text-align: center;
+    font-size: 1.6rem;
+    font-weight: 700;
+    line-height: 2;
+    /* margin: 30px auto 20px; */
+}
+</style>
---
+++ src/components/test/slot.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+
+</script> 
+
+<template>
+    <div>
+        <!-- 子組件的內容 -->
+        <slot name="certain-slot">
+            <p>123</p>
+        </slot>
+        <slot name="certain-slot2">
+            <p>456</p>
+        </slot>
+        <slot>unnamed slot</slot>
+    </div>
+</template>
+
+<style>
+    p {
+        width: 100%;
+        display: inline-block;
+        text-align: center;
+        font-size: 1.2rem;
+        font-weight: 700;
+        line-height: 1.6;
+        margin: 20px auto;
+    }
+    button{
+        text-align: center;
+        margin: 0 auto;
+    }
+</style>
